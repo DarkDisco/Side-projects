a4e0a97b926c54fe0dc3dfa79a08e2427e4d5cc0
Chris Meayers <42620840+MeayersCJ@users.noreply.github.com>
2018-09-04 11:56:45 +0100

    Update solvingexactcase.py

--- 1.0 Academic Projects/1.3 Estimating the quantum tunnelling probability of a given potential barrier/solvingexactcase.py
+++ 1.0 Academic Projects/1.3 Estimating the quantum tunnelling probability of a given potential barrier/solvingexactcase.py
@@ -12,7 +12,7 @@ hbar=1        #1.05457e-34
 V0=10             # Regtangualr potential barrier magnitude
 m=2       #9.11e-31        # mass of an electron
 T=1           # fix transmission amplitude 
-K=1              # set K equal to 1 for simplicity
+K=1              # Free particle wavenumber K equal to 1 for simplicity
 
 def V(x):
     "Calculates potential"
@@ -54,8 +54,8 @@ def runkut(n,x,y,h):
     
 
 #--------- setting inital conditions   
-xmax=400             #number of steps
-x=5; y=[0,T,0.0]   # y,y1,y2
+xmax=400             #number of steps/ upper limit of range
+x=5; y=[0,T,0.0]   # positive x position so that integration is in the negative direction;  y,y1,y2 
 
 
 #------- plotting 
@@ -86,12 +86,12 @@ plt.plot(xp,r)      # solutions for y2
 #plt.legend(('Phi', 'Phi Prime'))
 plt.xlabel('x [au]')
 plt.ylabel('Psi(x) [au]')
-someX, someY = 0, 0                  # Adds rectangle for barrier
+someX, someY = 0, 0                  # Adds rectangle for barrier graphic on graph
 currentAxis = plt.gca()
 currentAxis.add_patch(Rectangle((someX - 0.0, someY - 20), 1, 40,
                       alpha=1, facecolor='r'))
 
-plt.savefig("example of wavefunction2.pdf", bbox_inches="tight")
+plt.savefig("example of wavefunction2.pdf", bbox_inches="tight")  # saves graph
 
 #------------  working out transmission coeffienct
 
@@ -111,33 +111,7 @@ for j in range(0,xmax):   # works out T for corresponding x value
     Tcoefficent[j]=1-(Rflux[j]/Iflux[j])        # 1=RR*/II*
     t2[j]=Tflux[j]/Iflux[j]                   # TT*/II*
 
-#------------------- Plotting transmission coefficent 
-
-#plt.plot(Tflux)
-"""plt.plot(Tcoefficent)
-plt.legend('Transmission coef.')
-plt.show()"""
-
-
-"""plt.plot(Rcoefficent)
-plt.legend('Refletion coefficent')
-plt.show()"""
-
-Iflux1=[0 for i in range(0,200)]         #cuts the solution list to region 
-Rflux1= [0 for i in range(0,200)]        # before the barrier
-plotv= [0 for i in range(0,200)]
-xp1= [0 for i in range(0,200)]
-for j in range(0,200):
-    n=200
-    Iflux1[j]=Iflux[j+n]
-    Rflux1[j]=Rflux[j+n]
-    xp1[j]=xp[j+n]
-    n+=1
-    
-for i in range(0,200):
-    plotv[i]= 1-(Rflux1[i]/Iflux1[i])
-#plt.plot(xp1,plotv)
-avg=sum(Tcoefficent)/len(Tcoefficent)
+avg=sum(Tcoefficent)/len(Tcoefficent) # average transmission coefficent 
 print avg
 
 
@@ -149,11 +123,7 @@ else:
 
 print Ttheory"""
 
-y=[1,2,3,4,5,6]
-for i in range(0,3):
-    y.pop(0)
-    
-print y
+
 
 
 
